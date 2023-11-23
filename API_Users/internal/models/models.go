724ea7771f6ea1ef870fd505eebc3a81af1ed77d
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-11-23 17:47:35 +0100

    Update models.go

--- API_Users/internal/models/models.go
+++ API_Users/internal/models/models.go
@@ -8,3 +8,8 @@ type Collection struct {
 	Id      *uuid.UUID `json:"id"`
 	Content string     `json:"content"`
 }
+
+type User struct {
+	Id       *uuid.UUID `json:"id"`
+	Username string     `json:"username"`
+}
